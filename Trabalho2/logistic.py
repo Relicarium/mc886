3a81d2d1b0de192c7ce6076956afe984a45a9b1f
relicarium <figueiredo.carlos02@gmail.com>
2017-10-10 21:48:53 +0000

    oi

--- Trabalho2/logistic.py
+++ Trabalho2/logistic.py
@@ -3,12 +3,12 @@
 
 import numpy
 import sys
-from PIL import Image
+from PIL import Image, ImageFilter
 from numpy import genfromtxt
 from sklearn import linear_model, datasets
 
 classe = 0			# O que eu to tentando reconhecer
-numImages = 1000	# Número de imagens para treinar a rede
+numImages = 1000	# Numero de imagens para treinar a rede
 nInputs = 3073		# dimensao da imagem
 nTestes = 1000		# Numero de imagens de teste
 
@@ -16,13 +16,13 @@ nTestes = 1000		# Numero de imagens de teste
 # Definindo o conjunto de testes
 if (len(sys.argv) > 1):
 	if (sys.argv[1] == 'small'):
-		addr = './dataset.small'
-		print("Rodando com o Small Dataset - número de imagens = 1000")
+		addr = './dataset.noUp'
+		print("Rodando com o Small Dataset - numero de imagens = 1000")
 		numImages = 1000
-		nTestes = 1000
+		nTestes = 10000
 
 	elif (sys.argv[1] == 'full'):
-		addr = './dataset.small'
+		addr = './dataset.noUp'
 		print("WOOHOOOO RODANDO COM TODOS OS DADOOOS")
 		numImages = 50000
 		nTestes = 10000
@@ -38,7 +38,7 @@ if (len(sys.argv) > 1):
 			else:
 				nTestes = 5000
 		else:
-			print("Usando número default de imagens = 10000")
+			print("Usando numero default de imagens = 10000")
 			numImages = 10000
 			nTestes = 1000
 
@@ -55,42 +55,41 @@ print("Dataset: " + addr)
 print("\n---------------------------\n")
 
 print("Criando X de treino...")
-X = numpy.ones((numImages, 3073))
+X = numpy.ones((numImages, 3072))
 for num in range(0, numImages):
     im = Image.open(addr + '/train/' + '{:05d}'.format(num) + '.png')
+    im = im.filter(ImageFilter.EMBOSS)
     im = numpy.array(im).flatten()
-    X[num, 1:3073] = (im-127)/255
+    X[num] = (im - 127) / 255
 
 
 print("Criando Y de treino...")
-labelsT = genfromtxt(addr + '/train/labels', delimiter=',')
-y = numpy.zeros((numImages))
+y_in = genfromtxt(addr + '/train/labels', delimiter=',')
+y = y_in[0:numImages] 
 
-for num in range(0,numImages):
-    if labelsT[num] == classe:
-        y[num] = 1
 
 print("Modelando a rede...")
-mlp = MLPClassifier(hidden_layer_sizes=(1500),solver='sgd',learning_rate_init=0.01,max_iter=300,verbose=True)
+
+logreg = linear_model.LogisticRegression(solver = 'sag', multi_class = 'multinomial',C = 10e-5, verbose = 1, tol = 1e-20, max_iter=30)
 print("Treinando...")
-mlp.fit(X, y)
+logreg.fit(X, y)
 print("Treinado!")
 
 print("Criando X de teste...")
-X_test = numpy.ones((nTestes, 3073))
+X_test = numpy.ones((nTestes, 3072))
 for num in range(0, nTestes):
     im = Image.open(addr + '/test/' + '{:05d}'.format(num) + '.png')
+    im = im.filter(ImageFilter.EMBOSS)
     im = numpy.array(im).flatten()
-    X_test[num, 1:3073] = im
-X_test = (X_test-127)/255
+    X_test[num] = (im - 127) / 255
 
 print("Criando Y de teste...")
-labelsTest = genfromtxt(addr + '/test/labels', delimiter=',')
-y_test = numpy.zeros((nTestes))
-
-for num in range(0,nTestes):
-    if labelsTest[num] == classe:
-        y_test[num] = 1
-
+y_test_in = genfromtxt(addr + '/test/labels', delimiter=',')
+y_test = y_test_in[0:nTestes]
+y_predict = logreg.predict(X_test)
+numpy.savetxt('output1.txt', y_predict)
+numpy.savetxt('output2.txt', y_test)
 print("Testando")
-print (mlp.score(X_test, y_test))
+print(logreg.score(X_test, y_test))
+print(logreg.score(X, y))
+
